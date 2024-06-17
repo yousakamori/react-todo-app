2f9a0e1763843d3c89beba744d20c14a60c53097
yousakamori <yousakamori@gmail.com>
2024-06-18 07:54:00 +0900

    dummyデータの表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { dummyTodoList } from "./data/dummyTodoList";
+
 function App() {
   return (
     <main className="max-w-xl mx-auto mt-10">
       <h1 className="text-4xl text-center">Todoアプリ</h1>
+      <div className="p-5 rounded bg-slate-200">
+        <div className="space-y-3">
+          {dummyTodoList.map((todo) => (
+            <p key={todo.id}>{todo.title}</p>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
