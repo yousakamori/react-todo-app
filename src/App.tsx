9961d8f3ee71be8823cbe98112b6cd24998cb73b
yousakamori <yousakamori@gmail.com>
2024-06-18 07:57:02 +0900

    TodoList componentを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TodoList } from "./components/TodoList";
 import { dummyTodoList } from "./data/dummyTodoList";
 
 function App() {
@@ -5,11 +6,7 @@ function App() {
     <main className="max-w-xl mx-auto mt-10">
       <h1 className="text-4xl text-center">Todoアプリ</h1>
       <div className="p-5 rounded bg-slate-200">
-        <div className="space-y-3">
-          {dummyTodoList.map((todo) => (
-            <p key={todo.id}>{todo.title}</p>
-          ))}
-        </div>
+        <TodoList todoList={dummyTodoList} />
       </div>
     </main>
   );
